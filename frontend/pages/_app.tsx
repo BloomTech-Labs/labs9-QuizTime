c0ce070104e77e6f85a1e684c49ac21a98891d9a
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2019-01-10 17:34:06 -0600

    Mistake forgot to save now it has been pulled from development

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,38 +1,5 @@
 import App, { Container } from "next/app";
 import { ThemeProvider } from "emotion-theming";
-<<<<<<< HEAD
-import theme from "../utils/theme.ts";
-import NavBar from "../components/navbar/navbar";
-import SideBar from "../components/sidebar/sidebar";
-import AddBox from "../components/boxes/addBox/addBox";
-import QuizBox from "../components/boxes/quizBox/quizBox";
-import ClassBox from "../components/boxes/classBox/classBox";
-import styled from "@emotion/styled";
-const Holder = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-class QuizTime extends App {
-  render() {
-    const { Component } = this.props;
-    return (
-      <Container>
-        <ThemeProvider theme={theme}>
-          <NavBar />
-          <SideBar />
-          <Holder>
-            <ClassBox />
-            <QuizBox />
-            <QuizBox />
-            <AddBox />
-          </Holder>
-        </ThemeProvider>
-      </Container>
-    );
-  }
-=======
 import { ApolloProvider } from "react-apollo";
 import theme from "../utils/theme";
 import withData from "../utils/withData";
