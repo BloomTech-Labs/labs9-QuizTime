119359b152fa2f9a603d5397eadafb68aecedf0e
Allen Hai <allenhai03@gmail.com>
2019-01-10 16:43:02 -0600

    setup apollo client dependencies, but getting an error

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,18 +1,31 @@
 import App, { Container } from "next/app";
 import { ThemeProvider } from "emotion-theming";
+import { ApolloProvider } from "react-apollo";
 import theme from "../utils/theme";
+import withData from "../utils/withData";
 
 class QuizTime extends App {
+	static async getInitialProps({ Component, ctx }) {
+		let pageProps: any = {};
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
+		// this exposes the query to the user
+		pageProps.query = ctx.query;
+		return { pageProps };
+	}
 	render() {
-		const { Component } = this.props;
+		const { Component, pageProps, apollo } = this.props;
 		return (
 			<Container>
-				<ThemeProvider theme={theme}>
-					<Component />
-				</ThemeProvider>
+				<ApolloProvider client={apollo}>
+					<ThemeProvider theme={theme}>
+						<Component />
+					</ThemeProvider>
+				</ApolloProvider>
 			</Container>
 		);
 	}
 }
 
-export default QuizTime;
+export default withData(QuizTime);
