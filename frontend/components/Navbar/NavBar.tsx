3b615a4387a6ccf37677656cf55c1eb9a07e01bf
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2019-01-11 13:32:42 -0600

    Finished the initial Design for the NavBar component

--- frontend/components/Navbar/NavBar.tsx
+++ frontend/components/Navbar/NavBar.tsx
@@ -3,38 +3,39 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 import { Box } from "@rebass/emotion";
 const NavBarHolder = styled(Box)`
-    display: flex;
-    justify-content: flex-end;
-    align-items: center; 
-    background: #e16973;
-    margin-bottom: 5px;
-    border-bottom: 10px solid #ea969d;
-`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background: #e16973;
+  margin-bottom: 5px;
+  border-bottom: 10px solid #ea969d;
+`;
 const NavBarItem = styled.a`
-    padding: 20px;
-    cursor: pointer;
-    font-family: "system-ui";
-    color: white;
-`
+  padding: 20px;
+  cursor: pointer;
+  font-family: "system-ui";
+  color: white;
+`;
 const Avatar = styled.div`
-    margin: 15px;
-    width: 30px;
-    height: 30px;
-    border-radius: 30px; 
-    background: #152338; 
-    display: flex; 
-    justify-content: center;
-    align-items: center;
-`
-
+  margin: 15px;
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  background: #152338;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const NavBar: React.SFC = () => {
-    return (
-        <>
-        <NavBarHolder>
-            <NavBarItem>Sign Out</NavBarItem>
-            <Avatar><NavBarItem>K</NavBarItem></Avatar>
-        </NavBarHolder>
-        </>
-    )
-}
-export default NavBar; 
+  return (
+    <>
+      <NavBarHolder>
+        <NavBarItem>Sign Out</NavBarItem>
+        <Avatar>
+          <NavBarItem>K</NavBarItem>
+        </Avatar>
+      </NavBarHolder>
+    </>
+  );
+};
+export default NavBar;
