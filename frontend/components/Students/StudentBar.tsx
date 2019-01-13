d080167783809afc5c327c25e497920e06443c95
Allen Hai <allenhai03@gmail.com>
2019-01-13 09:50:19 -0600

    add hover styles on css prop for StudentBar to demonstrate babel plugin working

--- frontend/components/Students/StudentBar.tsx
+++ frontend/components/Students/StudentBar.tsx
@@ -1,7 +1,14 @@
 import { Text, Box } from "@rebass/emotion";
 
 export default ({ id, student }) => (
-    <Box m={5}>
+    <Box
+        m={5}
+        css={{
+            "&:hover": {
+                border: `1px solid lightgrey`
+            }
+        }}
+    >
         <Text color="red1" py={2} fontWeight={700} fontFamily="sans">
             {student.first_name} {student.last_name}
         </Text>
