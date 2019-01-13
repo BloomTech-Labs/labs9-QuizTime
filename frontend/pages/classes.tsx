68eac42d0d6025004871dbb7a5df670950806c8c
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2019-01-13 12:51:58 -0600

    The classes now appear on the classes page, just testing the color out, chose a yellow.

--- frontend/pages/classes.tsx
+++ frontend/pages/classes.tsx
@@ -1,1 +1,41 @@
-export default () => <div>Classes</div>;
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import styled from "@emotion/styled";
+import ClassBox from "../components/boxes/classBox/classBox";
+
+const ALL_CLASSES_QUERY = gql`
+	query ALL_CLASSES_QUERY {
+		class{
+			id
+			name
+		}
+	}
+`;
+const CardHolder = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`;
+
+const Holder = styled.div`
+	width: 1060px;
+	height: auto;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: flex-end;
+`;
+
+export default () => (
+	<CardHolder>
+		<Holder>
+			<Query query={ALL_CLASSES_QUERY}>
+				{({ loading, error, data }) => {
+					if (error) return <p>{error.message}</p>;
+					if (loading) return <p>...loading</p>;
+					if (data) {
+						return data.class.map(className => <ClassBox className={className} />);
+					}
+				}}
+			</Query>
+		</Holder>
+	</CardHolder>
+);
