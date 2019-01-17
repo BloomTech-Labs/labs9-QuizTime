deb003d1239b5e59713095c0bc76411d2ba4aecf
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2019-01-17 15:58:49 -0500

    Signed in checks if teacher exists before redirecting

--- frontend/pages/auth/signed-in.js
+++ frontend/pages/auth/signed-in.js
@@ -1,20 +1,49 @@
 import React from 'react'
 import Router from 'next/router'
+import { GraphQLClient } from 'graphql-request'
+import Cookie from 'js-cookie'
 
-import { setToken } from '../../utils/auth'
+import { setToken, getIdToken } from '../../utils/auth'
 import { parseHash } from '../../utils/auth0'
+
+const endpoint = `https://quiztime-hasura.herokuapp.com/v1alpha1/graphql`
+
+
+const queryTeacherCheck = `
+  query {
+    teacher {
+      id
+    }
+  }
+`
 
 export default class SignedIn extends React.Component {
   componentDidMount() {
     parseHash((err, result) => {
       if(err) {
         console.error('Something happened with the Sign In request')
+        //look into redirecting them to an error page
         return;
       }
 
-      setToken(result.idToken, result.accessToken);
-      Router.push('/home')
+      setToken(result.idToken, result.accessToken)
+      const client = new GraphQLClient(endpoint, {
+        headers: {
+          'Authorization': `Bearer ${result.idToken}`
+        }
+      })
+      console.log(client)
+      client.request(queryTeacherCheck).then(({ teacher }) => console.log(teacher))
     })
+
+
+
+    //make a query on teacher
+
+    //if results === 0
+    //insert a teacher
+
+    // Router.push('/home')
   }
   render(){
     return null
