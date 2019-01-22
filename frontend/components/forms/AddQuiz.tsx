73e2fa3ac70d3414d7be001bbbc27635de0e2a65
Allen Hai <allenhai03@gmail.com>
2019-01-22 13:35:15 -0600

    updated state with major question prompt

--- frontend/components/forms/AddQuiz.tsx
+++ frontend/components/forms/AddQuiz.tsx
@@ -31,6 +31,17 @@ class AddQuiz extends Component {
     }));
   };
 
+  updateMajorQuestion = (id, e) => {
+    console.log(`id ${id}:`, e.target.value);
+    const { value } = e.target;
+    const { questions } = this.state;
+    const updatedQuestions = questions.map(q => {
+      if (q.id == id) q.prompt = value;
+      return q;
+    });
+    this.setState(s => ({ ...s, questions: updatedQuestions }));
+  };
+
   generateMutation = () => {
     return gql`
       mutation insert_quiz {
@@ -84,7 +95,10 @@ class AddQuiz extends Component {
                     <Label htmlFor={`major-question-${q.id}`}>
                       Major Question {q.id}
                     </Label>
-                    <TextArea id={`major-question-${q.id}`} />
+                    <TextArea
+                      id={`major-question-${q.id}`}
+                      onChange={e => this.updateMajorQuestion(q.id, e)}
+                    />
                   </div>
                 ))}
                 <Button variant="primary" type="submit">
