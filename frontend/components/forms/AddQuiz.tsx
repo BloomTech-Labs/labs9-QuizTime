464b52a44c288d5ad3d922967ea3f1698d425da7
Allen Hai <allenhai03@gmail.com>
2019-01-22 14:28:19 -0600

    add delete major question functionality

--- frontend/components/forms/AddQuiz.tsx
+++ frontend/components/forms/AddQuiz.tsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import { Form, Input, Button, Label, Text, TextArea } from "../design-system";
+import { Box, Flex } from "@rebass/emotion";
 
 class AddQuiz extends Component {
   state = {
     name: "",
-    questions: [
-      { id: 1, prompt: "", answers: [{ response: "", correct: false }] }
+    majorQuestions: [
+      {
+        id: 1,
+        prompt: "",
+        answers: [{ response: "", correct: false }],
+        minorQuestions: []
+      }
     ]
   };
 
@@ -16,14 +22,14 @@ class AddQuiz extends Component {
     this.setState({ [name]: value });
   };
 
-  addQuestion = e => {
+  addMajorQuestion = e => {
     e.preventDefault();
     this.setState(s => ({
       ...s,
-      questions: [
-        ...s.questions,
+      majorQuestions: [
+        ...s.majorQuestions,
         {
-          id: s.questions[s.questions.length - 1]["id"] + 1,
+          id: s.majorQuestions[s.majorQuestions.length - 1]["id"] + 1,
           prompt: "",
           answers: [{ response: "", correct: false }]
         }
@@ -31,15 +37,23 @@ class AddQuiz extends Component {
     }));
   };
 
+  deleteMajorQuestion = id => {
+    const { majorQuestions } = this.state;
+    const updatedQuestions = majorQuestions.filter(q => {
+      if (q.id != id || q.id == 1) return q;
+    });
+    this.setState(s => ({ ...s, majorQuestions: updatedQuestions }));
+  };
+
   updateMajorQuestion = (id, e) => {
     console.log(`id ${id}:`, e.target.value);
     const { value } = e.target;
-    const { questions } = this.state;
-    const updatedQuestions = questions.map(q => {
+    const { majorQuestions } = this.state;
+    const updatedQuestions = majorQuestions.map(q => {
       if (q.id == id) q.prompt = value;
       return q;
     });
-    this.setState(s => ({ ...s, questions: updatedQuestions }));
+    this.setState(s => ({ ...s, majorQuestions: updatedQuestions }));
   };
 
   generateMutation = () => {
@@ -61,7 +75,7 @@ class AddQuiz extends Component {
   };
 
   render() {
-    const { questions } = this.state;
+    const { majorQuestions } = this.state;
     return (
       <Mutation mutation={this.generateMutation()}>
         {(insert_quiz, { error, loading, data }) => (
@@ -90,21 +104,75 @@ class AddQuiz extends Component {
                     onChange={this.handleChange}
                   />
                 </Label>
-                {questions.map(q => (
-                  <div key={q.id}>
-                    <Label htmlFor={`major-question-${q.id}`}>
-                      Major Question {q.id}
-                    </Label>
+                {majorQuestions.map(q => (
+                  <Box key={q.id} my={4}>
+                    <Flex justifyContent="space-around">
+                      <Label htmlFor={`major-question-${q.id}`}>
+                        Major Question {q.id}
+                      </Label>
+                      <Button
+                        variant="error"
+                        p={0}
+                        fontSize={0}
+                        onClick={e => this.deleteMajorQuestion(q.id)}
+                      >
+                        Delete
+                      </Button>
+                    </Flex>
                     <TextArea
                       id={`major-question-${q.id}`}
                       onChange={e => this.updateMajorQuestion(q.id, e)}
                     />
-                  </div>
+                    <Box>
+                      <Label>Answer 1</Label>
+                      <Flex>
+                        <Input my={3} width={1 / 2} />
+                        <Input
+                          type="radio"
+                          name={`major-question-${q.id}-major-answer`}
+                          value="1"
+                        />
+                      </Flex>
+                    </Box>
+                    <Box>
+                      <Label>Answer 2</Label>
+                      <Flex>
+                        <Input my={3} width={1 / 2} />
+                        <Input
+                          type="radio"
+                          name={`major-question-${q.id}-major-answer`}
+                          value="2"
+                        />
+                      </Flex>
+                    </Box>
+                    <Box>
+                      <Label>Answer 3</Label>
+                      <Flex>
+                        <Input my={3} width={1 / 2} />
+                        <Input
+                          type="radio"
+                          name={`major-question-${q.id}-major-answer`}
+                          value="3"
+                        />
+                      </Flex>
+                    </Box>
+                    <Box>
+                      <Label>Answer 4</Label>
+                      <Flex>
+                        <Input my={3} width={1 / 2} />
+                        <Input
+                          type="radio"
+                          name={`major-question-${q.id}-major-answer`}
+                          value="4"
+                        />
+                      </Flex>
+                    </Box>
+                  </Box>
                 ))}
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
-                <Button variant="success" onClick={this.addQuestion}>
+                <Button variant="success" onClick={this.addMajorQuestion}>
                   Add Major Question
                 </Button>
               </fieldset>
