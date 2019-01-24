6dc123b7116aa74425704b3e3bde01b309f250dc
Carey <careycbaldwin@gmail.com>
2019-01-24 10:17:07 -0500

    mapping over state data to display quiz questions

--- frontend/components/StudentView/StudentQuiz.tsx
+++ frontend/components/StudentView/StudentQuiz.tsx
@@ -16,18 +16,30 @@ import { Box, Flex } from "@rebass/emotion";
   
 let dummyData = {
     name: "Lambda Quiz",
+    details:  "This quiz is designed to assess your understaning of the procedures and expectaions of being a student at Lambda School",
     majorQuestions: [
         {
             id: 1,
-            prompt: "Which of the following is not a daily requirement of Lambda School",
+            prompt: "Which of the following is not a daily requirement of Lambda School?",
             answers: [
-                { response: "airtable", correct: false },
-                { response: "DM instructor", correct: true },
-                { response:  "camera on", correct: false },
-                { response: "code challenge", correct: false  }
+                { label: "a", response: "airtable", correct: false },
+                { label: "b", response: "DM instructor", correct: true },
+                { label: "c", response:  "camera on", correct: false },
+                { label: "d", response: "code challenge", correct: false  }
             ],
             minorQuestions: []
-        }
+        },
+        {
+            id: 2,
+            prompt: "If you have a questions, who should you reach out to first?",
+            answers: [
+                { label: "a", response: "Instructor", correct: false },
+                { label: "b", response: "Austin", correct: false },
+                { label: "c", response: "Yer Mom", correct: false },
+                { label: "d", response: "Project Manager", correct: true  }
+            ],
+            minorQuestions: []
+        },
     ],
 };
 
@@ -37,22 +49,41 @@ class StudentQuiz extends Component {
     state = dummyData;
     
     render() {
-        return(
-            
-    <>
-    {/* we will need to pull in the quiz data from the database and render it as a quiz */}
-    {/* will need to map over the quiz questions and render a box for each */}   
-    {console.log('state', this.state.name)}
-    {console.log('prompt', this.state.majorQuestions[0].prompt)}  
-    {console.log('answers', this.state.majorQuestions[0].answers)} 
-    {console.log('first answer correct?', this.state.majorQuestions[0].answers[0].correct)} 
-    <Box b="1px solid black">
-    <BoxText><UpperCase>Quiz Title</UpperCase></BoxText>
-    <BoxText><UpperCase>Question 1</UpperCase></BoxText>
-    <BoxText><UpperCase>Prompt</UpperCase></BoxText>
-    <BoxText><UpperCase>Answer Choices</UpperCase></BoxText>
-    </Box>
-    </>
+        const { majorQuestions } = this.state;
+        return( 
+
+            <>
+            {/* we will need to pull in the quiz data from the database and render it as a quiz */}
+            {/* will need to map over the quiz questions and render a box for each */}   
+            {/* {console.log('state', this.state.name)}
+            {console.log('prompt', this.state.majorQuestions[0].prompt)}  
+            {console.log('answers', this.state.majorQuestions[0].answers)} 
+            {console.log('first answer correct?', this.state.majorQuestions[0].answers[0].correct)}  */}
+            <Box m={4}>
+            {/* need to get class name from props? */}
+            <BoxText fontSize ><UpperCase>{this.state.name}</UpperCase></BoxText>
+            <BoxText>{this.state.details}</BoxText>
+            </Box>
+
+            {majorQuestions.map(q => (
+                <Box m={4} key={q.id}>
+                    <BoxText htmlFor={`major-question-${q.id}`}>
+                        <UpperCase>Question {q.id}</UpperCase>
+                    </BoxText>
+                    <BoxText>
+                       Prompt {q.prompt}
+                    </BoxText>
+                    {console.log('q.answers', q.answers)}
+                    {q.answers.map(a => (
+                    <Box>
+                        <BoxText>{a.label}. {a.response}</BoxText>
+                    </Box>
+                    ))}
+                </Box>
+            ))}
+
+        
+            </>
     )};
 };
 
