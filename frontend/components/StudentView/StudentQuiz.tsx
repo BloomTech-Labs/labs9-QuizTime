7f1f24e42b596d5e3797f02e384a92bd60064dda
Carey <careycbaldwin@gmail.com>
2019-01-24 08:40:35 -0500

    added some dummy data and figured out references in state

--- frontend/components/StudentView/StudentQuiz.tsx
+++ frontend/components/StudentView/StudentQuiz.tsx
@@ -11,23 +11,47 @@ import {
     BoxHolder,
     Button
   } from "../design-system";
+
+import { Box, Flex } from "@rebass/emotion";
   
+let dummyData = {
+    name: "Lambda Quiz",
+    majorQuestions: [
+        {
+            id: 1,
+            prompt: "Which of the following is not a daily requirement of Lambda School",
+            answers: [
+                { response: "airtable", correct: false },
+                { response: "DM instructor", correct: true },
+                { response:  "camera on", correct: false },
+                { response: "code challenge", correct: false  }
+            ],
+            minorQuestions: []
+        }
+    ],
+};
+
 
 class StudentQuiz extends Component {
-    state = {
-      majorQuestions: [],
-    };
-
+    //For development purposes.  Can remove later after retrieving data.
+    state = dummyData;
+    
     render() {
         return(
             
     <>
     {/* we will need to pull in the quiz data from the database and render it as a quiz */}
-    {/* will need to map over the quiz questions and render a box for each */}        
+    {/* will need to map over the quiz questions and render a box for each */}   
+    {console.log('state', this.state.name)}
+    {console.log('prompt', this.state.majorQuestions[0].prompt)}  
+    {console.log('answers', this.state.majorQuestions[0].answers)} 
+    {console.log('first answer correct?', this.state.majorQuestions[0].answers[0].correct)} 
+    <Box b="1px solid black">
     <BoxText><UpperCase>Quiz Title</UpperCase></BoxText>
     <BoxText><UpperCase>Question 1</UpperCase></BoxText>
     <BoxText><UpperCase>Prompt</UpperCase></BoxText>
     <BoxText><UpperCase>Answer Choices</UpperCase></BoxText>
+    </Box>
     </>
     )};
 };
