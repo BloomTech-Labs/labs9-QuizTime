5565a51132da48ddc61cfb436c73d942c7175d5c
Carey <careycbaldwin@gmail.com>
2019-01-25 04:46:20 -0500

    small addition of 'StudentViewNav' to primitives

--- frontend/components/design-system/primitives.ts
+++ frontend/components/design-system/primitives.ts
@@ -43,6 +43,11 @@ const StyledText = styled.a`
   ${fontSize}
 `;
 
+export const StudentViewNav = props => (
+  <F  
+    justifyContent="flex-end" {...props} />
+);
+
 export const BoxText = props => (
   <T
     StyledText
