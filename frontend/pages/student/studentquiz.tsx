db56a3031d3e046530b7692640820c5d38cb96e0
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-01-29 15:01:11 +0100

    eliminates redudanct jsx in studentquiz page

--- frontend/pages/student/studentquiz.tsx
+++ frontend/pages/student/studentquiz.tsx
@@ -1,24 +1,21 @@
 import Link from "next/link";
 
 import {
-    Button,
-    Container, 
-  } from "../../components/design-system";
-  
+  Button,
+  Container,
+} from "../../components/design-system";
+
 import StudentQuiz from "../../components/StudentView/StudentQuiz";
 
-  
-  const TakeQuiz = props => (
-        <>
-            <Container>
-                <Link href="/student" prefetch>
-                    <Button variant = "primary" m={2}>Back to Profile</Button>
-                </Link>
-                <Button variant = "success" m={2}>Email Teacher</Button>
-            {/* StudentQuiz component should show the quiz the student is supposed to take */}
-                <StudentQuiz />
-            </Container>
-        </>
-  );
-  
-  export default TakeQuiz;
+
+const TakeQuiz = props => (
+  <Container>
+    <Link href="/student" prefetch>
+      <Button variant="primary" m={2}>Back to Profile</Button>
+    </Link>
+    <Button variant="success" m={2}>Email Teacher</Button>
+    <StudentQuiz />
+  </Container>
+);
+
+export default TakeQuiz;
