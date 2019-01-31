8058db6f111b5ea7f0ce637af24c20a41648772d
Allen Hai <allenhai03@gmail.com>
2019-01-30 22:41:42 -0600

    render links based on device size

--- frontend/components/Navbar/NavBar.tsx
+++ frontend/components/Navbar/NavBar.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import { Box, Button } from "@rebass/emotion";
-import { NavBarHolder, AvatarImg } from "../design-system/primitives";
+import { Box, Button, Flex } from "@rebass/emotion";
+import { NavBarHolder, AvatarImg, BoldText } from "../design-system/primitives";
 import { unsetToken, getUserFromLocalCookie } from "../../utils/auth";
 import { logout } from "../../utils/auth0";
+import { useMedia } from "the-platform";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 // const NavBarHolder = styled(Box)`
 //   display: flex;
@@ -32,8 +35,26 @@ const NavBarItem = styled.a`
 
 const user = getUserFromLocalCookie();
 const NavBar: React.SFC = () => {
+  const small = useMedia("(max-width: 639px)");
+
+  const Links = small ? (
+    <Flex width={1}>
+      <Box mx={3}>
+        <Link href="/classes">
+          <BoldText color="white">classes</BoldText>
+        </Link>
+      </Box>
+      <Box mx={3}>
+        <Link href="/quizzes">
+          <BoldText color="white">quizzes</BoldText>
+        </Link>
+      </Box>
+    </Flex>
+  ) : null;
+
   return (
     <NavBarHolder>
+      {Links}
       <AvatarImg
         src={user && user.picture ? user.picture : null}
         alt="profile"
@@ -41,4 +62,5 @@ const NavBar: React.SFC = () => {
     </NavBarHolder>
   );
 };
+
 export default NavBar;
--- frontend/components/SideBar.tsx
+++ frontend/components/SideBar.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import styled from "@emotion/styled";
 import Link from "next/link";
-import { Box } from "@rebass/emotion";
+import { Box, Flex } from "@rebass/emotion";
 import { SideBarHolder, UpperCase } from "./design-system/primitives";
+import { useMedia } from "the-platform";
 
 const BoxText = styled.a`
   font-family: "system-ui";
@@ -17,17 +18,28 @@ const BoxText = styled.a`
 `;
 
 const SideBar: React.SFC = () => {
+  const large = useMedia("(min-width: 640px)");
   return (
     <SideBarHolder>
-      <Link href="/" prefetch>
-        <BoxText>QuizTime</BoxText>
-      </Link>
-      <Link href="/quizzes" prefetch>
-        <UpperCase color="white">quizzes</UpperCase>
-      </Link>
-      <Link href="/classes" prefetch>
-        <UpperCase color="white">classes</UpperCase>
-      </Link>
+      {large && (
+        <Flex width={1} flexDirection="column" alignItems="center">
+          <Box mb={4}>
+            <Link href="/" prefetch>
+              <BoxText>QuizTime</BoxText>
+            </Link>
+          </Box>
+          <Box my={3}>
+            <Link href="/quizzes" prefetch>
+              <UpperCase color="white">quizzes</UpperCase>
+            </Link>
+          </Box>
+          <Box my={3}>
+            <Link href="/classes" prefetch>
+              <UpperCase color="white">classes</UpperCase>
+            </Link>
+          </Box>
+        </Flex>
+      )}
     </SideBarHolder>
   );
 };
