6a4132883146640b5039852da0445af9b05e55ad
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-01-31 22:17:44 +0100

    fixes width of button to be symmetric

--- frontend/components/LandingPage/GetStarted.tsx
+++ frontend/components/LandingPage/GetStarted.tsx
@@ -1,7 +1,7 @@
 //carey
-import React from "react";
-import { css } from '@emotion/core'
-import { Box, Flex } from "@rebass/emotion";
+import React from 'react';
+import { css } from '@emotion/core';
+import { Box, Flex } from '@rebass/emotion';
 import {
   Container,
   Text,
@@ -11,14 +11,14 @@ import {
   FlexColumns,
   LandingText,
   HeaderText,
-  BoxText
-} from "../../components/design-system";
-import { authorize } from "../../utils/auth0";
+  BoxText,
+} from '../../components/design-system';
+import { authorize } from '../../utils/auth0';
 const GetStarted = props => {
   return (
     <>
       <Box my={5}>
-        <LandingText color="blue.1" fontSize={6} css={{ textAlign: "center" }}>
+        <LandingText color='blue.1' fontSize={6} css={{ textAlign: 'center' }}>
           Getting Started
         </LandingText>
         <FlexColumns>
@@ -26,99 +26,117 @@ const GetStarted = props => {
             my={3}
             mx={5}
             p={4}
-            bg="blue.2"
-            width="360px"
+            bg='blue.2'
+            width='360px'
             css={{
-              border: "1px solid white",
-              height: "360px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center"
+              border: '1px solid white',
+              height: '360px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
+              alignItems: 'center',
             }}
           >
-            <LandingText fontWeight={3} color="white" fontSize={5} p={2}>
+            <LandingText fontWeight={3} color='white' fontSize={5}>
               Free Plan
             </LandingText>
-            <LandingText
-              fontWeight={3}
-              fontSize={4}
-              color="white"
-              lineHeight={1.5}
-              p={4}
+            <Box>
+              <LandingText
+                fontWeight={3}
+                fontSize={[3, 4]}
+                color='white'
+                lineHeight={1.5}
+              >
+                Sign up now and make ten quizzes for free!
+              </LandingText>
+            </Box>
+            <Box
+              css={{
+                width: '100%',
+                '&:hover': {
+                  transform: 'scale(1.05)',
+                  boxShadow: '0px 5px 20px rgba(0,0,0,0.50)',
+                },
+              }}
             >
-              Sign up now and make ten quizzes for free!
-            </LandingText>
+              <HeaderText
+                fontSize={[4,5]}
+                onClick={authorize}
+                bg='green.1'
+                color='blue.1'
+                css={css`
+                  cursor: pointer;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                `}
+              >
+                Try Now!
+              </HeaderText>
+            </Box>
           </Box>
           <Box
             my={3}
             mx={5}
             p={4}
-            bg="blue.2"
-            width="360px"
+            bg='blue.2'
+            width='360px'
             css={{
-              border: "1px solid white",
-              height: "360px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center"
+              border: '1px solid white',
+              height: '360px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
+              alignItems: 'center',
             }}
           >
             <LandingText
-            fontWeight={3}
-            color="white" 
-            fontSize={5}
-            p={2}
-              css={css`
-                @media(max-width:500px){
-                  position:relative;
-                  bottom:-50px;
-                }
-              `}
+              fontWeight={3}
+              color='white'
+              fontSize={5}
+              // css={css`
+              //   @media (max-width: 500px) {
+              //     position: relative;
+              //     bottom: -50px;
+              //   }
+              // `}
             >
               Pro Plan
             </LandingText>
-            <LandingText
-              fontWeight={3}
-              fontSize={4}
-              color="white"
-              lineHeight={1.5}
-              p={4}
-              css={css`
-                @media(max-width:500px){
-                  position:relative;
-                  bottom:-15px;
-                }
-              `}
-            >
-              Get unlimited quizzes for a low monthly fee of $9.95.
-              <Box css={{
+            <Box>
+              <LandingText
+                fontWeight={3}
+                fontSize={[3, 4]}
+                color='white'
+                lineHeight={1.5}
+              >
+                Get unlimited quizzes for a low monthly fee of $9.95.
+              </LandingText>
+            </Box>
+            <Box
+              css={{
+                width: '100%',
                 '&:hover': {
                   transform: 'scale(1.05)',
-                  boxShadow: '0px 5px 20px rgba(0,0,0,0.50)'
-                }
-              }}>
+                  boxShadow: '0px 5px 20px rgba(0,0,0,0.50)',
+                },
+              }}
+            >
               <HeaderText
-                fontSize={5}
+                fontSize={[4,5]}
                 onClick={authorize}
-                bg="green.1"
-                color="blue.1"
+                bg='green.1'
+                color='blue.1'
                 css={css`
                   cursor: pointer;
                   display: flex;
-                  justify-content:center;
-                  align-items:center;
-                  @media(max-width:500px){
-                    position:relative;
-                    bottom:-45px;
-                  }
+                  justify-content: center;
+                  align-items: center;
                 `}
               >
                 Get Started
               </HeaderText>
-              </Box>
-            </LandingText>
+            </Box>
           </Box>
         </FlexColumns>
       </Box>
