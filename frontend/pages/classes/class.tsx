d8cc8b5a040ae9e0b9217bb81c6f388a1aaff29d
Carey <careycbaldwin@gmail.com>
2019-02-04 10:20:33 -0500

    switched quiz management to right side to be consistent with classes and quizzes

--- frontend/pages/classes/class.tsx
+++ frontend/pages/classes/class.tsx
@@ -36,15 +36,40 @@ const ClassPage = ({ query: { id } }) => {
           if (data) {
             return (
               // page content containers
-              <Box p={3} m={3} >
+              <Box p={3} mx={3} >
                 <Flex>
-                  <Box py={3} my={3} width={[1, 1, 1/4]}>
-                  <Label my={2} >Quiz Management</Label>
+                  {/* container for left side */}
+                  <Box p={2} m={3} width={[1, 1, 3 / 4]}>
+                    <Flex
+                      flexDirection="column"
+                    >
+                      {/* <Box p={3} m={3}>
+                        <AddStudent class_id={id} />
+                      </Box> */}
+                      <Box p={2}>
+                        <Label mx={2} my={3} >Class Management</Label>
+                        <Flex flexWrap="wrap">
+                          <ATag>
+                            <AddBox />
+                          </ATag>
+                          {data.class[0].students.map(student => (
+                            <StudentBox
+                              id={student.id}
+                              key={student.id}
+                              student={student}
+                            />
+                          ))}
+                        </Flex>
+                      </Box>
+                    </Flex>
+                  </Box>
+                  <Box py={3} m={3} width={[1, 1, 1 / 4]}>
+                    <Label mx={2} my={3} >Quiz Management</Label>
                     <Flex
                       flexDirection="column"
                     >
                       {/*quizzes you can use*/}
-                      <Box p={3} my={3} 
+                      <Box p={3} my={3}
                         css={{ background: "white", boxShadow: "0px 3px 15px rgba(0,0,0,0.1)" }}>
                         <UpperCase fontSize={2} fontWeight={4}>Add Quiz to Class</UpperCase>
                         {data.quiz
@@ -70,33 +95,6 @@ const ClassPage = ({ query: { id } }) => {
                           ))}
                         </Box>
                       </Box>
-                    </Flex>
-                  </Box>
-                  {/* container for right side */}
-                  <Box p={2} m={3} width={[1, 1, 3/4]} css={{ boxShadow: "0px 3px 15px rgba(0,0,0,0.1)" }}>
-                    <Flex
-                      flexDirection="column"
-                    >
-                      {/* <Box p={3} m={3}>
-                        <AddStudent class_id={id} />
-                      </Box> */}
-                      <Box p={2}>
-                        <Label m={2} >Class Management</Label>
-                        <Flex flexWrap="wrap">
-                          <ATag>
-                            <AddBox />
-                          </ATag>
-                          {data.class[0].students.map(student => (
-                            <StudentBox
-                              id={student.id}
-                              key={student.id}
-                              student={student}
-                            />
-                          ))}
-                        </Flex>
-                      </Box>
-                      {/* container for quiz information */}
-
                     </Flex>
                   </Box>
                 </Flex>
