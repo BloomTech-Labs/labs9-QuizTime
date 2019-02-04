3e839b1a6a4ddc8e35f72dbd28112c89c324d585
keith Kennedy <keith.kennedy@valpo.edu>
2019-02-04 13:29:34 -0600

    styled the back of the card, now instead of hover it needs to be a click that triggers the flip animation

--- frontend/components/boxes/studentBox/studentBox.tsx
+++ frontend/components/boxes/studentBox/studentBox.tsx
@@ -16,70 +16,78 @@ const StudentBox = ({ id, student }) => {
   return (
     <>
       <BoxHolder
-      css={css`
-      border-bottom:5px solid #70e89d;
-      position:relative;
-      transform-style: preserve-3d;
-      transition: all 600ms;
-      &:hover{
-        transform:rotateX(180deg);
-      }
-
-      `}
+        css={css`
+          border-bottom: 5px solid #70e89d;
+          position: relative;
+          transform-style: preserve-3d;
+          transition: all 600ms;
+          &:hover {
+            transform: rotateX(180deg);
+          }
+        `}
       >
-      <div
-      css={css`
-      position:relative;
-      backface-visibility:hidden;
-      `}>
-          <Box
-           m={1}
-           >
-            <Flex
-            flexDirection="row"
-            justifyContent="space-between"
-            >
-            <BoxText fontWeight={1}>
-              {" "}
-              {student.first_name} {student.last_name}
-            </BoxText>
-            <UniButton fontSize={0} bg="blue.2">Delete</UniButton>
-            </Flex>
-            </Box>
-            <Box
-            p={1}
-            css={css`
-            border-top:1px solid #B5FFD0;
-            `}
-            >
-            <BoxText fontWeight={1}>
-            {student.email}
-            </BoxText>
-            <Flex
-            flexDirection="row"
-            justifyContent="space-between"
-            mt={1}
-            p={1}
-            css={css`
-            border-top:1px solid #B5FFD0;
-            `}
-            >
-            <UniButton bg="green.1" fontSize={0}>Edit</UniButton>
-            <UniButton bg="blue.5" fontSize={0}>View Results</UniButton>
-            </Flex>
-          </Box>
-          </div>
-          <Box
-          bg="blue.0"
+        <div
           css={css`
-          transform: rotateX(180deg);
-          backface-visibility:hidden;
+            position: relative;
+            backface-visibility: hidden;
           `}
-          >
-            <BoxText>Back</BoxText>
-            <BoxText>Back</BoxText>
-
+        >
+          <Box m={1}>
+            <Flex flexDirection="row" justifyContent="space-between">
+              <BoxText fontWeight={1}>
+                {" "}
+                {student.first_name} {student.last_name}
+              </BoxText>
+              <UniButton fontSize={0} bg="blue.2">
+                Delete
+              </UniButton>
+            </Flex>
           </Box>
+          <Box
+            p={1}
+            css={css`
+              border-top: 1px solid #b5ffd0;
+            `}
+          >
+            <BoxText fontWeight={1}>{student.email}</BoxText>
+            <Flex
+              flexDirection="row"
+              justifyContent="space-between"
+              mt={1}
+              p={1}
+              css={css`
+                border-top: 1px solid #b5ffd0;
+              `}
+            >
+              <UniButton bg="green.1" fontSize={0}>
+                Edit
+              </UniButton>
+              <UniButton bg="blue.5" fontSize={0}>
+                View Results
+              </UniButton>
+            </Flex>
+          </Box>
+        </div>
+        <div
+          css={css`
+            transform: rotateX(180deg);
+            backface-visibility: hidden;
+          `}
+        >
+          <Box justifyContent="flex-start">
+            <BoxText fontWeight={1}>Quiz Results</BoxText>
+          </Box>
+          <Box
+          p={1}
+            css={css`
+              border-top: 1px solid #b5ffd0;
+            `}
+          >
+          <Flex flexDirection="column" css={css`border-bottom:1px solid #b5ffd0;`}>
+            <BoxText fontWeight={1}>Alg Quiz: 11/14</BoxText>
+            </Flex>
+          </Box>
+        </div>
       </BoxHolder>
     </>
   );
