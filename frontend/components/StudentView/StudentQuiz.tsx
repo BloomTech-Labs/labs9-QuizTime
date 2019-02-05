82993a5579c9bf7392adec5332d4a2f9fc83e7b8
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-05 18:42:54 +0100

    sets url to deployed value

--- frontend/components/StudentView/StudentQuiz.tsx
+++ frontend/components/StudentView/StudentQuiz.tsx
@@ -19,8 +19,8 @@ import QuizHeading from './QuizHeading.tsx';
 import MajorQuestion from './MajorQuestion';
 import MinorQuestion from './MinorQuestion';
 
-const url = 'http://localhost:7000/api/student-proxy';
-// const url = '/api/student-proxy';
+// const url = 'http://localhost:7000/api/student-proxy';
+const url = '/api/student-proxy';
 
 class StudentQuiz extends Component {
   state = {
