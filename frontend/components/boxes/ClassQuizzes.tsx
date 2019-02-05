3d7a9ce8f5841d2dbd898717f173315d2c6fa5f8
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-05 16:53:59 +0100

    datepicker ui implemented; need to craft mutation; due_date changed to date on class_quiz table and set to timestamp type

--- frontend/components/boxes/ClassQuizzes.tsx
+++ frontend/components/boxes/ClassQuizzes.tsx
@@ -6,25 +6,22 @@ import DatePicker from 'react-datepicker';
 
 const ClassQuizzes: React.SFC = ({ quiz, classId}) => {
   const [quizDate, setQuizDate] = useState(null);
-  console.log('quiz', quiz, classId)
-  // useEffect(()=> {
-  //   const selectedDate = `${quizDate.getFullYear()}-${quizDate.getMonth()}-${quizDate.getDate()}`
-  // })
+  // console.log('quiz', quiz, classId)
 
   const handleQuizDate = (date, id) => {
     if (quizDate) {
       const selectedDate = `${quizDate.getFullYear()}-${quizDate.getMonth()}-${quizDate.getDate()}`
     }
 
-    console.log('what does handle get?', date, quiz.id, classId)
+    // console.log('what does handle get?', date, quiz.id, classId)
     setQuizDate(date)
   }
   return (
     <>
       <Box>
-        <Flex>
+        <Flex justifyContent='space-between'>
         <Text my={2}>{quiz.name}</Text>
-        <DatePicker style={{maxWidth: '80px'}} selected={quizDate} placeholderText='Select quiz email date' onChange={(date) => handleQuizDate(date, quiz.id)} />
+        <DatePicker selected={quizDate} placeholderText='Select quiz email date' onChange={(date) => handleQuizDate(date, quiz.id)} />
         </Flex>
       </Box>
     </>
