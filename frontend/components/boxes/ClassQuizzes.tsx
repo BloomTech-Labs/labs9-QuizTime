78435eef136fb19d3ba60e5405df9e2f61bba6d3
Carey <careycbaldwin@gmail.com>
2019-02-05 15:36:54 -0500

    polished quiz management styles

--- frontend/components/boxes/ClassQuizzes.tsx
+++ frontend/components/boxes/ClassQuizzes.tsx
@@ -1,11 +1,19 @@
-import {QuizBar, Text} from "../design-system/primitives";
-import {Box, Flex} from "@rebass/emotion";
+import { QuizBar, Text, UniButton } from "../design-system/primitives";
+import { Box, Flex } from "@rebass/emotion";
 
-const ClassQuizzes = ({quiz}) => (
-    <>
-      <Box>
-        <Text my={2}>{quiz.name}</Text>
-      </Box>
-    </>
+const ClassQuizzes = ({ quiz }) => (
+  <>
+    <Box my={3} css={{ borderBottom: "1px solid lightgray" }}>
+      <Flex
+        justifyContent="space-between"
+        alignItems="flex-end"
+      >
+        <Text m={2}>{quiz.name}</Text>
+        <Box>
+          <UniButton m={2} bg="green.1" fontSize={0}>Assign</UniButton>
+        </Box>
+      </Flex>
+    </Box>
+  </>
 )
 export default ClassQuizzes
