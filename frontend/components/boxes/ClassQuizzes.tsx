6d86c9f563771473d2eb967ec83516b671665ce9
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-05 21:56:52 +0100

    testing mutation

--- frontend/components/boxes/ClassQuizzes.tsx
+++ frontend/components/boxes/ClassQuizzes.tsx
@@ -3,25 +3,42 @@ import { useState, useEffect } from 'react';
 import { QuizBar, Text } from '../design-system/primitives';
 import { Box, Flex } from '@rebass/emotion';
 import DatePicker from 'react-datepicker';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
-const ClassQuizzes: React.SFC = ({ quiz, classId}) => {
+const UPDATE_CLASS_QUIZ = gql`
+mutation update_class_quiz($date: String!)
+`
+const ClassQuizzes: React.SFC = ({ quiz, classId }) => {
   const [quizDate, setQuizDate] = useState(null);
-  // console.log('quiz', quiz, classId)
+
+  // useEffect(()=> {
+
+  // })
 
   const handleQuizDate = (date, id) => {
     if (quizDate) {
-      const selectedDate = `${quizDate.getFullYear()}-${quizDate.getMonth()}-${quizDate.getDate()}`
+      const selectedDate = `${quizDate.getFullYear()}-${quizDate.getMonth()}-${quizDate.getDate()}`;
     }
 
     // console.log('what does handle get?', date, quiz.id, classId)
-    setQuizDate(date)
-  }
+    setQuizDate(date);
+  };
   return (
     <>
       <Box>
         <Flex justifyContent='space-between'>
-        <Text my={2}>{quiz.name}</Text>
-        <DatePicker selected={quizDate} placeholderText='Select quiz email date' onChange={(date) => handleQuizDate(date, quiz.id)} />
+          <Box my={2}>
+            <Text>{quiz.name}</Text>
+          </Box>
+          <Box my={2}>
+            <DatePicker
+              selected={quizDate}
+              placeholderText='Assign email date'
+              onChange={date => handleQuizDate(date, quiz.id)}
+              popperPlacement='bottom-start'
+            />
+          </Box>
         </Flex>
       </Box>
     </>
