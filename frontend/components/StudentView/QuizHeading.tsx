0522c837b6ffc7a0653e46a66e595697bb1747f4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-06 19:36:04 +0100

    adds personalized welcome greeting to student quiz view

--- frontend/components/StudentView/QuizHeading.tsx
+++ frontend/components/StudentView/QuizHeading.tsx
@@ -2,19 +2,29 @@ import { Box } from '@rebass/emotion';
 import { UpperCase, BoxText } from '../design-system';
 
 const QuizHeading = props => {
-  const {quiz, student} = props
+  const { quiz, student } = props;
   return (
     <Box mx={2} my={3} width={0.95}>
       <BoxText fontSize={5}>
-        {student && `Welcome ${student.first_name[0].toUpperCase().concat(student.first_name.slice(1))} ${student.last_name[0].toUpperCase().concat(student.last_name.slice(1))}!`} 
-        <hr/><br/>
+        {student &&
+          `Welcome ${student.first_name[0]
+            .toUpperCase()
+            .concat(
+              student.first_name.slice(1)
+            )} ${student.last_name[0]
+            .toUpperCase()
+            .concat(student.last_name.slice(1))}!`}
+        <hr />
+        <br />
       </BoxText>
       <BoxText>
         <UpperCase fontSize={5} fontWeight={4}>
           {quiz.name}
         </UpperCase>
       </BoxText>
-      <BoxText fontSize={3} css={{lineHeight: 1.25}}>{quiz.description}</BoxText>
+      <BoxText fontSize={3} css={{ lineHeight: 1.25 }}>
+        {quiz.description}
+      </BoxText>
     </Box>
   );
 };
