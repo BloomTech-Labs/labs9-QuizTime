43b489a8f376949656c0b275887956e6c94c246a
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-07 19:53:26 +0100

    fixes console error on selected prop-type in datepicker

--- frontend/components/boxes/ClassQuizzes.tsx
+++ frontend/components/boxes/ClassQuizzes.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import { useState, useEffect } from 'react';
 import { QuizBar, Text, ButtonLink } from '../design-system/primitives';
 import { Box, Flex } from '@rebass/emotion';
@@ -8,11 +8,13 @@ import { Mutation } from 'react-apollo';
 import { UPDATE_CLASS_QUIZ } from '../../mutations';
 
 const ClassQuizzes: React.SFC = ({ quiz, classId, dueDate }) => {
-  const [quizDate, setQuizDate] = useState(dueDate);
+  const dueDateOfTypeDate = new Date(dueDate)
+  const [quizDate, setQuizDate] = useState(dueDateOfTypeDate);
 
   const handleQuizDate = (date, id, update) => {
     //* Weird ... month begins at 0 ... so + 1 ... hack
-    const selectedDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+    const selectedDate = `${date.getFullYear()}-${date.getMonth() +
+      1}-${date.getDate()}`;
     console.log('selectedDate', selectedDate, quiz.id, classId);
     update({
       variables: {
@@ -27,15 +29,19 @@ const ClassQuizzes: React.SFC = ({ quiz, classId, dueDate }) => {
   return (
     <>
       <Box>
-        <Flex justifyContent='space-between' alignItems='center' flexDirection='row'>
+        <Flex
+          justifyContent='space-between'
+          alignItems='center'
+          flexDirection='row'
+        >
           <Box mr={4} my={2}>
             <Text>{quiz.name}: </Text>
           </Box>
-          <Box >
+          <Box>
             <Mutation mutation={UPDATE_CLASS_QUIZ}>
               {(update_class_quiz, { error, loading, data }) => (
                 <DatePicker
-                customInput={<CustomInput/>}
+                  customInput={<CustomInput />}
                   selected={quizDate}
                   onChange={date =>
                     handleQuizDate(date, quiz.id, update_class_quiz)
@@ -59,16 +65,16 @@ const ClassQuizzes: React.SFC = ({ quiz, classId, dueDate }) => {
 export default ClassQuizzes;
 
 class CustomInput extends React.Component {
-
-  render () {
+  render() {
     return (
       <ButtonLink
-        style={{padding: '2px 5px'}}
+        style={{ padding: '2px 5px' }}
         fontSize={'12px'}
         fontWeight={'600'}
-        onClick={this.props.onClick}>
+        onClick={this.props.onClick}
+      >
         {this.props.value || 'Assign'}
       </ButtonLink>
-    )
+    );
   }
 }
