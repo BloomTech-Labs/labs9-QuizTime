194af8854dc8188fba7edb2d308bed7d50b8affb
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-07 04:19:05 +0100

    adds custom styling to reactdatepicker input element

--- frontend/components/boxes/ClassQuizzes.tsx
+++ frontend/components/boxes/ClassQuizzes.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react';
+// import * as React from 'react';
+import React, {Component} from 'react'
 import { useState, useEffect } from 'react';
-import { QuizBar, Text } from '../design-system/primitives';
+import { QuizBar, Text, ButtonLink } from '../design-system/primitives';
 import { Box, Flex } from '@rebass/emotion';
 import DatePicker from 'react-datepicker';
 import gql from 'graphql-tag';
@@ -32,7 +33,8 @@ const ClassQuizzes: React.SFC = ({ quiz, classId, dueDate }) => {
   const [quizDate, setQuizDate] = useState(dueDate);
 
   const handleQuizDate = (date, id, update) => {
-    const selectedDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    //* Weird ... month begins at 0 ... so + 1 ... hack
+    const selectedDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
     console.log('selectedDate', selectedDate, quiz.id, classId);
     update({
       variables: {
@@ -47,25 +49,21 @@ const ClassQuizzes: React.SFC = ({ quiz, classId, dueDate }) => {
   return (
     <>
       <Box>
-        <Flex justifyContent='space-between' flexDirection='column'>
+        <Flex justifyContent='space-between' alignItems='center' flexDirection='row'>
           <Box my={2}>
             <Text>{quiz.name}:</Text>
           </Box>
-          <Box my={2} alignSelf='center'>
+          <Box my={2}>
             <Mutation mutation={UPDATE_CLASS_QUIZ}>
               {(update_class_quiz, { error, loading, data }) => (
                 <DatePicker 
-                style={{height: '50px', fontSize: '18px'}}
+                customInput={<ExampleCustomInput/>}
                   selected={quizDate}
-                  placeholderText='Assign email date'
                   onChange={date =>
                     handleQuizDate(date, quiz.id, update_class_quiz)
                   }
                   popperPlacement='bottom'
                   popperModifiers={{
-                    flip: {
-                      enabled: false,
-                    },
                     preventOverflow: {
                       enabled: true,
                       escapeWithReference: false,
@@ -81,3 +79,20 @@ const ClassQuizzes: React.SFC = ({ quiz, classId, dueDate }) => {
   );
 };
 export default ClassQuizzes;
+
+class ExampleCustomInput extends React.Component {
+
+  render () {
+    return (
+      <ButtonLink
+        style={{height: 'auto', padding: '2px 5px'}}
+        fontSize={'12px'}
+        fontWeight={'600'}
+        p={0}
+        className="example-custom-input"
+        onClick={this.props.onClick}>
+        {this.props.value || 'Assign'}
+      </ButtonLink>
+    )
+  }
+}
